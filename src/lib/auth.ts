51165b2b402342d52ddd5ee34d0ab19734703a54
Tom Hewitt <tommhewitt@gmail.com>
2026-06-06 16:26:22 +0100

    chore: update package

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,15 +2,15 @@ const getTokenAuth = async () => {
   const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REFRESH_TOKEN } = process.env;
 
   const response = await fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers: {
-      Authorization: `Basic ${Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64')}`,
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
     body: new URLSearchParams({
       grant_type: 'refresh_token',
       refresh_token: `${SPOTIFY_REFRESH_TOKEN}`,
     }),
+    headers: {
+      Authorization: `Basic ${Buffer.from(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`).toString('base64')}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    method: 'POST',
   });
 
   return await response.json();
